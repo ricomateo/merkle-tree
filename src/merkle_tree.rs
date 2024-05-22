f6a42bdc91c0c06ca5853d2c5ef5bdd71350b325
ricomateo <mrico@fi.uba.ar>
2024-05-22 14:42:49 -0300

    format

--- src/merkle_tree.rs
+++ src/merkle_tree.rs
@@ -1,13 +1,11 @@
 use sha256::digest;
 
-
-
 #[derive(Debug, PartialEq)]
 pub struct MerkleTree {
     root: Node,
 }
 
-/// A (merkle tree) Node. 
+/// A (merkle tree) Node.
 /// Each node contains:
 ///   * hashed value,
 ///   * height in the tree (0 corresponds to a leaf),
@@ -24,14 +22,20 @@ fn hash(data: String) -> String {
     digest(data)
 }
 
-
 impl MerkleTree {
-
     /// Creates a merkle tree from a vector of strings
     pub fn new(elements: Vec<String>) -> Self {
-        let leafs = elements.iter().map(|e| Node{hash: hash(e.clone()), height: 0, left: None, right: None}).collect();
+        let leafs = elements
+            .iter()
+            .map(|e| Node {
+                hash: hash(e.clone()),
+                height: 0,
+                left: None,
+                right: None,
+            })
+            .collect();
         let root = Self::build_tree(leafs)[0].clone();
-        MerkleTree{root}
+        MerkleTree { root }
     }
 
     /// Takes a vector of nodes and builds the tree up to the root
@@ -50,12 +54,12 @@ impl MerkleTree {
         // For example, if we have the following nodes
         // nodes = [1, 2, 3, 4]
         // then (1, 2) is a pair, so we create their parent.
-        // the same goes for (3, 4). 
+        // the same goes for (3, 4).
         for i in (0..len).step_by(2) {
             let left_node = &nodes[i];
             let right_node = &nodes[i + 1];
 
-            let parent = Node{
+            let parent = Node {
                 hash: hash(left_node.hash.clone() + &right_node.hash.clone()),
                 height: left_node.height + 1,
                 left: Some(Box::new(left_node.clone())),
@@ -68,7 +72,6 @@ impl MerkleTree {
     }
 }
 
-
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -77,23 +80,61 @@ mod tests {
     /// it to a Merkle Tree created with the constructor
     #[test]
     fn merkle_tree_is_created_correctly() {
-        let elements = vec![String::from("A"), String::from("B"), String::from("C"), String::from("D")];
+        let elements = vec![
+            String::from("A"),
+            String::from("B"),
+            String::from("C"),
+            String::from("D"),
+        ];
 
-        let leaf_node_0 = Node{hash: hash(elements[0].clone()), height: 0, left: None, right: None};
-        let leaf_node_1 = Node{hash: hash(elements[1].clone()), height: 0, left: None, right: None};
-        let leaf_node_2 = Node{hash: hash(elements[2].clone()), height: 0, left: None, right: None};
-        let leaf_node_3 = Node{hash: hash(elements[3].clone()), height: 0, left: None, right: None};
-        
-        
-        let node01 = Node{hash: hash(leaf_node_0.hash.clone() + &leaf_node_1.hash), height: 1, left: Some(Box::new(leaf_node_0)), right: Some(Box::new(leaf_node_1))};
-        let node23 = Node{hash: hash(leaf_node_2.hash.clone() + &leaf_node_3.hash), height: 1, left: Some(Box::new(leaf_node_2)), right: Some(Box::new(leaf_node_3))};
-        let root = Node{hash: hash(node01.hash.clone() + &node23.hash), height: 2, left: Some(Box::new(node01)), right: Some(Box::new(node23))};
-        
-        let tree_by_hand = MerkleTree{root: root};
-        
+        let leaf_node_0 = Node {
+            hash: hash(elements[0].clone()),
+            height: 0,
+            left: None,
+            right: None,
+        };
+        let leaf_node_1 = Node {
+            hash: hash(elements[1].clone()),
+            height: 0,
+            left: None,
+            right: None,
+        };
+        let leaf_node_2 = Node {
+            hash: hash(elements[2].clone()),
+            height: 0,
+            left: None,
+            right: None,
+        };
+        let leaf_node_3 = Node {
+            hash: hash(elements[3].clone()),
+            height: 0,
+            left: None,
+            right: None,
+        };
+
+        let node01 = Node {
+            hash: hash(leaf_node_0.hash.clone() + &leaf_node_1.hash),
+            height: 1,
+            left: Some(Box::new(leaf_node_0)),
+            right: Some(Box::new(leaf_node_1)),
+        };
+        let node23 = Node {
+            hash: hash(leaf_node_2.hash.clone() + &leaf_node_3.hash),
+            height: 1,
+            left: Some(Box::new(leaf_node_2)),
+            right: Some(Box::new(leaf_node_3)),
+        };
+        let root = Node {
+            hash: hash(node01.hash.clone() + &node23.hash),
+            height: 2,
+            left: Some(Box::new(node01)),
+            right: Some(Box::new(node23)),
+        };
+
+        let tree_by_hand = MerkleTree { root: root };
+
         let tree = MerkleTree::new(elements);
-        
+
         assert_eq!(tree_by_hand, tree);
-         
     }
 }
